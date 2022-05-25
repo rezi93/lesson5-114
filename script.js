de4ec909b48097cc24d8f6c637f4cd5b8662f0eb
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-05-25 22:38:28 +0400

    add html js css

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+let burger =document.getElementById('burgerbar');
+let navmenu =document.getElementById('navigation');
+
+burger.addEventListener('click', function(){
+    navmenu.classList.toggle('active')
+});
