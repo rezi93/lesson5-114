1307dcfc115cccd36c88ee815606998a832c410a
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-05-26 22:00:11 +0400

    add js css

--- script.js
+++ script.js
@@ -3,7 +3,22 @@
 
 let burger =document.getElementById('burgerbar');
 let navmenu =document.getElementById('navigation');
+let line = document.getElementById('item');
+
 
 burger.addEventListener('click', function(){
-    navmenu.classList.toggle('active')
-});
+    navmenu.classList.toggle('open');
+   
+})
+// burger.addEventListener('click', function(){
+//     line.classList.toggle('open');
+   
+// })
+// let menu btn =document.querySelectorall('.burger');
+// menu btn.addEventListener('click', function(){
+//     menu btn.classList.toggle('is-active');
+// })
+
+document.getElementById('user').addEventListener('focus', function(event){
+    event.target.style.background = 'green';
+})
